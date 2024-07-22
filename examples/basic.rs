1162d015793320f8a5509a002777dcdfcf218ee3
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2024-07-21 19:47:49 -0600

    exampler

---
+++ examples/basic.rs
@@ -0,0 +1,214 @@
+use bevy::{color::palettes::basic::*, prelude::*};
+use bevy_la_mesa::events::{DeckShuffle, DrawHand};
+use bevy_la_mesa::utils::{load_poker_deck, PokerCard};
+use bevy_la_mesa::{DeckArea, LaMesaPlugin, LaMesaPluginSettings};
+
+// // Main
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(LaMesaPlugin::<PokerCard>::default())
+        .add_systems(Startup, (setup, setup_ui))
+        .add_systems(Update, (button_system,))
+        .insert_resource(LaMesaPluginSettings::<PokerCard> {
+            num_players: 1,
+            deck: load_poker_deck(),
+        })
+        .run();
+}
+
+/// set up lights and scene
+fn setup(mut commands: Commands) {
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+#[derive(Component)]
+pub struct ButtonShuffleDeck;
+
+#[derive(Component)]
+pub struct ButtonDrawHand;
+
+pub fn button_system(
+    mut set: ParamSet<(
+        Query<
+            (
+                &Interaction,
+                &mut BackgroundColor,
+                &mut BorderColor,
+                &Children,
+                &ButtonShuffleDeck,
+            ),
+            (Changed<Interaction>, With<Button>),
+        >,
+        Query<
+            (
+                &Interaction,
+                &mut BackgroundColor,
+                &mut BorderColor,
+                &Children,
+                &ButtonDrawHand,
+            ),
+            (Changed<Interaction>, With<Button>),
+        >,
+    )>,
+    decks: Query<(Entity, &DeckArea)>,
+    mut text_query: Query<&mut Text>,
+    mut ew_shuffle: EventWriter<DeckShuffle>,
+    mut ew_draw: EventWriter<DrawHand>,
+) {
+    if decks.iter().count() == 0 {
+        return;
+    }
+
+    let deck_entity = decks.iter().next().unwrap().0;
+
+    for (interaction, mut color, mut border_color, children, _) in &mut set.p0().iter_mut() {
+        let mut _text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                // text.sections[0].value = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = RED.into();
+
+                ew_shuffle.send(DeckShuffle { deck_entity });
+            }
+            Interaction::Hovered => {
+                // text.sections[0].value = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                // text.sections[0].value = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+
+    for (interaction, mut color, mut border_color, children, _) in &mut set.p1().iter_mut() {
+        let mut _text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                // text.sections[0].value = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = RED.into();
+
+                ew_draw.send(DrawHand {
+                    deck_entity,
+                    num_cards: 5,
+                    player: 1,
+                });
+            }
+            Interaction::Hovered => {
+                // text.sections[0].value = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                // text.sections[0].value = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
+
+pub fn setup_ui(mut commands: Commands, _sasset_server: Res<AssetServer>) {
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Px(65.0),
+                    align_items: AlignItems::Start,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+            Name::new("UI"),
+        ))
+        .with_children(|parent| {
+            // Shuffle
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Px(350.0),
+                            height: Val::Px(65.0),
+                            border: UiRect::all(Val::Px(5.0)),
+                            // horizontally center child text
+                            justify_content: JustifyContent::Center,
+                            // vertically center child text
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        border_color: BorderColor(Color::BLACK),
+                        border_radius: BorderRadius::MAX,
+                        background_color: NORMAL_BUTTON.into(),
+                        ..default()
+                    },
+                    ButtonShuffleDeck,
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "barajar cartas",
+                        TextStyle {
+                            // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::srgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+
+            // Draw hands
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Px(350.0),
+                            height: Val::Px(65.0),
+                            border: UiRect::all(Val::Px(5.0)),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        border_color: BorderColor(Color::BLACK),
+                        border_radius: BorderRadius::MAX,
+                        background_color: NORMAL_BUTTON.into(),
+                        ..default()
+                    },
+                    ButtonDrawHand,
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "dibujar la mano",
+                        TextStyle {
+                            // font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::srgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        });
+}
---
+++ src/lib.rs
@@ -0,0 +1,200 @@
+pub mod events;
+pub mod utils;
+
+use bevy::prelude::*;
+use bevy_mod_picking::{
+    events::*, picking_core::Pickable, prelude::On, DefaultPickingPlugins, PickableBundle,
+};
+use bevy_tweening::TweeningPlugin;
+use events::*;
+use std::{fmt::Debug, marker::PhantomData};
+
+pub trait CardMetadata {
+    type Output;
+
+    fn filename(&self) -> String;
+}
+
+#[derive(Component)]
+pub struct Card<CardType> {
+    pub pickable: bool,
+    pub transform: Option<Transform>,
+    pub data: CardType,
+}
+
+#[derive(Component)]
+pub struct DeckArea;
+
+#[derive(Component)]
+pub struct Deck;
+
+#[derive(Component)]
+pub struct HandArea {
+    pub player: usize,
+}
+
+#[derive(Component)]
+pub struct Hand {
+    pub player: usize,
+}
+
+#[derive(Default, Resource)]
+pub struct LaMesaPluginSettings<T: Send + Clone + Sync + Debug + CardMetadata + 'static> {
+    pub num_players: usize,
+    pub deck: Vec<T>,
+}
+
+#[derive(Default)]
+pub struct LaMesaPlugin<T: Send + Clone + Sync + Debug + CardMetadata + 'static>(
+    pub PhantomData<T>,
+);
+
+impl<T: Send + Clone + Sync + Debug + CardMetadata + 'static> Plugin for LaMesaPlugin<T> {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(LaMesaPluginSettings::<T> {
+            num_players: 1,
+            deck: vec![],
+        })
+        .add_systems(Startup, (render_deck::<T>, render_hands_area::<T>))
+        .add_systems(
+            Update,
+            (
+                handle_card_hover::<T>,
+                handle_card_out::<T>,
+                handle_deck_shuffle::<T>,
+                handle_draw_hand::<T>,
+                handle_card_press::<T>,
+            ),
+        )
+        .add_plugins((DefaultPickingPlugins, TweeningPlugin))
+        .add_event::<CardHover>()
+        .add_event::<CardOut>()
+        .add_event::<CardPress>()
+        .add_event::<DeckShuffle>()
+        .add_event::<DrawHand>()
+        .add_event::<MoveCardToHand>();
+    }
+}
+
+pub const DECK_WIDTH: f32 = 5.0 * 2.6;
+
+pub fn render_hands_area<T>(mut commands: Commands)
+where
+    T: Send + Clone + Sync + Debug + 'static,
+{
+    commands
+        .spawn((
+            Name::new("HandArea"),
+            TransformBundle {
+                local: Transform::from_translation(Vec3::new(0.0, 0.0, 3.7))
+                    .with_rotation(Quat::from_rotation_x(std::f32::consts::PI / 4.0)),
+                ..default()
+            },
+            HandArea { player: 1 },
+        ))
+        .with_children(|_parent| {
+            // parent.spawn()
+        });
+}
+
+pub fn render_deck<T>(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    plugin_settings: Res<LaMesaPluginSettings<T>>,
+) where
+    T: Send + Clone + Sync + Debug + CardMetadata + 'static,
+{
+    // load deck
+    let card_deck = plugin_settings.deck.clone();
+    let deck_transform =
+        Transform::IDENTITY.with_rotation(Quat::from_rotation_x(std::f32::consts::PI / 2.0));
+
+    commands.spawn((
+        TransformBundle {
+            local: deck_transform,
+            ..default()
+        },
+        Name::new("Deck"),
+        DeckArea,
+        InheritedVisibility::default(),
+    ));
+
+    for (i, card) in card_deck.iter().enumerate() {
+        let face_texture = asset_server.load(card.clone().filename());
+        let face_material = materials.add(StandardMaterial {
+            base_color_texture: Some(face_texture.clone()),
+            ..Default::default()
+        });
+
+        let face_texture = asset_server.load("card-back1.png");
+        let back_material = materials.add(StandardMaterial {
+            base_color_texture: Some(face_texture.clone()),
+            ..Default::default()
+        });
+
+        let transform = Transform::from_translation(Vec3::new(0.0, 0.01 * (i as f32), 0.0))
+            .with_rotation(
+                Quat::from_rotation_x(std::f32::consts::PI / 2.0)
+                    * Quat::from_rotation_y(std::f32::consts::FRAC_PI_2)
+                    * Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)
+                    * Quat::from_rotation_y(std::f32::consts::FRAC_PI_2),
+            );
+
+        // Draw Deck
+        commands
+            .spawn((
+                Name::new("Card"),
+                Card {
+                    pickable: false,
+                    transform: None,
+                    data: card.clone(),
+                },
+                Deck,
+                PbrBundle {
+                    mesh: meshes.add(Plane3d::default().mesh().size(2.5, 3.5).subdivisions(10)),
+                    transform,
+                    ..default()
+                },
+                PickableBundle::default(),
+                On::<Pointer<Over>>::send_event::<CardHover>(),
+                On::<Pointer<Down>>::send_event::<CardPress>(),
+                On::<Pointer<Out>>::send_event::<CardOut>(),
+            ))
+            .with_children(|parent| {
+                // face
+                parent.spawn((
+                    PbrBundle {
+                        mesh: meshes.add(Plane3d::default().mesh().size(2.5, 3.5).subdivisions(10)),
+                        material: face_material,
+                        ..default()
+                    },
+                    PickableBundle {
+                        pickable: Pickable {
+                            is_hoverable: false,
+                            ..default()
+                        },
+                        ..default()
+                    },
+                ));
+                // back
+                parent.spawn((
+                    PbrBundle {
+                        mesh: meshes.add(Plane3d::default().mesh().size(2.5, 3.5).subdivisions(10)),
+                        material: back_material,
+                        transform: Transform::IDENTITY
+                            .with_rotation(Quat::from_rotation_z(std::f32::consts::PI)),
+                        ..default()
+                    },
+                    PickableBundle {
+                        pickable: Pickable {
+                            is_hoverable: false,
+                            ..default()
+                        },
+                        ..default()
+                    },
+                ));
+            });
+    }
+}
